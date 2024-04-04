3f23e1623a80427970fd39ca33f0d090d295d35c
linkzhang <1519815799@qq.com>
2024-04-04 08:35:49 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Link>
+</template>
+
+<script>
+import Link from "./components/Link.vue";
+
+export default {
+  name: "App",
+  components: {
+    Link,
+  },
+};
+</script>
