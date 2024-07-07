44f66f8e391d91b172dc19c7f7dc5ca0ac3a59ed
linkzhang <1519815799@qq.com>
2024-07-07 15:35:28 +0800

    支持 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -49,5 +49,23 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.link-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.link-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
 }
 </style>
