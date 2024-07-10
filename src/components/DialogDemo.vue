47014b9ac081caa5394ff9a89c3bb1a18c83b870
linkzhang <1519815799@qq.com>
2024-07-10 17:15:20 +0800

    Dialog 支持自定义 title 和 content

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,7 +2,20 @@
   <div>Dialog 示例</div>
   <h1>示例1</h1>
   <Button @click="toggle">toggle</Button>
-  <Dialog :visible="x"></Dialog>
+  <Dialog
+    v-model:visible="x"
+    :closeOnClickOverlay="false"
+    :ok="f1"
+    :cancel="f2"
+  >
+    <template v-slot:content>
+      <strong>hi1</strong>
+      <div>hi2</div>
+    </template>
+    <template v-slot:tittle>
+      <strong>加粗的标题</strong>
+    </template>
+  </Dialog>
 </template>
 <script lang="ts">
 import { ref } from "vue";
@@ -18,7 +31,11 @@ export default {
     const toggle = () => {
       x.value = !x.value;
     };
-    return { x, toggle };
+    const f1 = () => {
+      return false;
+    };
+    const f2 = () => {};
+    return { x, toggle, f1, f2 };
   },
 };
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,16 +1,18 @@
 <template>
   <template v-if="visible">
-    <div class="link-dialog-overlay"></div>
+    <div class="link-dialog-overlay" @click="onClickOverlay"></div>
     <div class="link-dialog-wrapper">
       <div class="link-dialog">
-        <header>标题 <span class="link-dialog-close"></span></header>
+        <header>
+          <slot name="tittle">
+          <span @click="close" class="link-dialog-close"></span>
+        </header>
         <main>
-          <p>第一行字</p>
-          <p>第二行字</p>
+          <slot name="content">
         </main>
         <footer>
-          <Button level="main">OK</Button>
-          <Button>Cancel</Button>
+          <Button level="main" @click="ok">OK</Button>
+          <Button @click="cancel">Cancel</Button>
         </footer>
       </div>
     </div>
@@ -24,9 +26,39 @@ export default {
       type: Boolean,
       default: false,
     },
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true,
+    },
+    ok: {
+      type: Function,
+    },
+    cancel: {
+      type: Function,
+    },
   },
   components: {
     Button,
+  },
+  setup(props, context) {
+    const close = () => {
+      context.emit("update:visible", false);
+    };
+    const onClickOverlay = () => {
+      if (props.closeOnClickOverlay) {
+        close();
+      }
+    };
+    const ok = () => {
+      if (props.ok?.() !== false) {
+        close();
+      }
+    };
+    const cancel = () => {
+      context.emit("cancel");
+      close();
+    };
+    return { close, onClickOverlay, ok, cancel };
   },
 };
 </script>
