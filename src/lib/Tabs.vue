0c5f4ce1c48f63e1c47d391320933b7f0a2a2be7
linkzhang <1519815799@qq.com>
2024-07-19 12:47:45 +0800

    实现下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="link-tabs">
-    <div class="link-tabs-nav">
+    <div class="link-tabs-nav" ref="container">
       <div
         class="link-tabs-nav-item"
         v-for="(t, index) in titles"
+        :ref="
+          (el) => {
+            if (el) navItems[index] = el;
+          }
+        "
         @click="select(t)"
         :class="{ selected: t === selected }"
         :key="index"
       >
         {{ t }}
       </div>
-      <div class="link-tabs-nav-indicator"></div>
+      <div class="link-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="link-tabs-content">
       <component :is="current" :key="current.props.title" />
@@ -20,7 +25,7 @@
 
 <script lang="ts">
 import Tab from "./Tab.vue";
-import { computed } from "vue";
+import { computed, onMounted, onUpdated, ref } from "vue";
 export default {
   props: {
     selected: {
@@ -28,6 +33,23 @@ export default {
     },
   },
   setup(props, context) {
+    const x = () => {
+      const divs = navItems.value;
+      const result = divs.filter((div) =>
+        div.classList.contains("selected")
+      )[0];
+      const { width } = result.getBoundingClientRect();
+      indicator.value.style.width = width + "px";
+      const { left: left1 } = container.value.getBoundingClientRect();
+      const { left: left2 } = result.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + "px";
+    };
+    const container = ref<HTMLDivElement>(null);
+    const indicator = ref<HTMLDivElement>(null);
+    const navItems = ref<HTMLDivElement[]>([]);
+    onMounted(x);
+    onUpdated(x);
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -46,7 +68,15 @@ export default {
     const select = (title: string) => {
       context.emit("update:selected", title);
     };
-    return { defaults, titles, current, select };
+    return {
+      defaults,
+      titles,
+      current,
+      select,
+      navItems,
+      indicator,
+      container,
+    };
   },
 };
 </script>
@@ -79,6 +109,7 @@ $border-color: #d9d9d9;
       left: 0;
       bottom: -1px;
       width: 100px;
+      transition: all 250ms;
     }
   }
   &-content {
