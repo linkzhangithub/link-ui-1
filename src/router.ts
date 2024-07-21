c46b45a6f3d2cb7fdcca1df3afe227d7cbd0bfb6
linkzhang <1519815799@qq.com>
2024-07-21 17:55:56 +0800

    创建路由

--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,10 @@ import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import DocDemo from './components/DocDemo.vue'
 
+import Intro from './views/Intro.vue'
+import Install from './views/Install.vue'
+import GetStarted from './views/GetStarted.vue'
+
 const history = createWebHashHistory()
 export const router = createRouter({
   history: history,
@@ -17,6 +21,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: '', component: DocDemo },
+        { path: 'intro', component: Intro },
+        { path: 'install', component: Install },
+        { path: 'get-started', component: GetStarted },
         { path: 'switch', component: SwitchDemo },
         { path: 'button', component: ButtonDemo },
         { path: 'dialog', component: DialogDemo },
