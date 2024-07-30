ebf006cae9321bcec76221905c2ecf4fcf74d15f
linkzhang <1519815799@qq.com>
2024-07-30 19:41:05 +0800

    解决页面拖拽时显示 bug

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,11 @@ export default {
   name: "App",
   setup() {
     const width = document.documentElement.clientWidth;
-    const menuVisible = ref(width <= 500 ? false : true);
+    const menuVisible = ref(width > 500);
     provide("menuVisible", menuVisible); //set
     router.afterEach(() => {
-      if (width <= 500) {
+      //这里的if判断不能用width作为参考，因为width是在setup函数中定义的常量，不会随着视图宽度变化而更新，如果用width后将页面横向拖拽，会有显示bug
+      if (document.documentElement.clientWidth <= 500) {
         menuVisible.value = false;
       }
     });
